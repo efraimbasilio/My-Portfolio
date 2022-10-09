2ac1255852555bd1fb4223ce84537158bb6f8714
Efraim Basilio <basilio.efraim@gmail.com>
2022-10-10 04:30:25 +0800

    Update Timeline section Text

--- src/components/TimeLine/TimeLine.js
+++ src/components/TimeLine/TimeLine.js
@@ -46,7 +46,7 @@ const Timeline = () => {
     <Section id="about">
       <SectionTitle>About Me</SectionTitle>
       <SectionText>
-      The purpose of JavaScript Mastery is to help aspiring and established developers to take their development skills to the next level and build awesome apps.
+      “I'm an experienced software engineer who constantly seeks out innovative solutions to everyday problems. In my years of experince, I've honed my analytical thinking and collaboration skills, and I love working with a team.
       </SectionText>
       <CarouselContainer ref={carouselRef} onScroll={handleScroll}>
         <>
