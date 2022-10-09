235dd63219495ff1e0c6651446b10e5d6f387b7a
Efraim Basilio <basilio.efraim@gmail.com>
2022-10-10 01:23:11 +0800

    Added Hero Section

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -5,9 +5,17 @@ import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Hello! <br />
+        I am Efraim 
+      </SectionTitle>
+      <SectionText>
+      Diligent software engineer with of experience in developing web application, with a hands-on, high-energy approach, well-developed skills in Agile methodology, and an unapologetically user-focused philosophy. Have developed several desktop and web apps.
+      </SectionText>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
