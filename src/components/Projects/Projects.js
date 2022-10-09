45a59b1235a4b4cce4dcd6d5963be5b2513fb6e1
Efraim Basilio <basilio.efraim@gmail.com>
2022-10-10 02:13:30 +0800

    Added Project Section
    Mapping content gets little bit tricky.
    
    Content:
    *Array inside an object
    *Need to have a two parameter for looping in each values. for object and array.

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,12 +2,37 @@ import React from 'react';
 
 import { BlogCard, CardInfo, ExternalLinks, GridContainer, HeaderThree, Hr, Tag, TagList, TitleContent, UtilityList, Img } from './ProjectsStyles';
 import { Section, SectionDivider, SectionTitle } from '../../styles/GlobalComponents';
-import { projects } from '../../constants/constants';
+import { projects, tags } from '../../constants/constants';
 
 const Projects = () => (
-  <div>
-    Projects
-  </div>
+  <Section nopadding>
+    <SectionDivider/>
+    <SectionTitle main>Projects</SectionTitle>
+     <GridContainer>
+       {projects.map((p,i) =>(
+         <BlogCard key={i}>
+          <Img src={p.image}/>
+          <TitleContent>
+            <HeaderThree title>{p.title}</HeaderThree>
+            <Hr/>
+          </TitleContent>
+          <CardInfo>{p.description}</CardInfo>
+          <div>
+            <TitleContent>Stack</TitleContent>
+            <TagList>
+              {p.tags.map((t,i)=>(
+                <Tag key={i}>{t}</Tag>
+              ))}
+            </TagList>
+          </div>
+          {/* <UtilityList>
+            <ExternalLinks href={p.visit}>Code</ExternalLinks>
+            <ExternalLinks href={p.source}>Source</ExternalLinks>
+          </UtilityList> */}
+         </BlogCard>
+       ))}
+     </GridContainer>
+  </Section>
 );
 
 export default Projects;
